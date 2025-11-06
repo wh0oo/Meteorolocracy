b2cebee9f99cb04730c5bd26b36e82f102993d39
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-11-06 10:34:34 -0600

    Change permission check method in VoteManager

--- src/main/java/com/wh0oo/meteorolocracy/VoteManager.java
+++ src/main/java/com/wh0oo/meteorolocracy/VoteManager.java
@@ -34,7 +34,7 @@ public class VoteManager {
         }
 
         if (!voteInProgress) {
-            boolean isOp = source.hasPermissionLevel(2);
+            boolean isOp = source.hasPermission(2);
 
             long lastUsed = lastVoteTimes.getOrDefault(playerId, 0L);
             long cooldownMillis = VoteConfig.getPlayerCooldown() * 1000L;
